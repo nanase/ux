6a50583d898ca7e91bbadbb0f02dfaf8b8db68f7
Tomona Nanase <nanase@users.noreply.github.com>
2013-12-03 04:23:20 +0900

    Extensionクラスのメンバの記述の整理

--- ux/Component/Extension.cs
+++ ux/Component/Extension.cs
@@ -8,132 +8,138 @@ namespace ux.Utils
 	/// 拡張メソッドを集めたクラスです。
 	/// </summary>
 	public static class ValueTypeEx
-	{
-		/// <summary>
-		/// Double 値を 0.0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static double Clamp (this double value, double max)
-		{
-			return value < 0.0 ? 0.0 : value > max ? max : value;
-		}
+    {
+        #region -- Public Static Methods --
+        #region Clamp
+        /// <summary>
+        /// Double 値を 0.0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static double Clamp(this double value, double max)
+        {
+            return value < 0.0 ? 0.0 : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// Double 値を 0.0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <param name="min">範囲の最小値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static double Clamp (this double value, double max, double min)
-		{
-			return value < min ? min : value > max ? max : value;
-		}
+        /// <summary>
+        /// Double 値を 0.0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <param name="min">範囲の最小値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static double Clamp(this double value, double max, double min)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// Single 値を 0.0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static float Clamp (this float value, float max)
-		{
-			return value < 0.0f ? 0.0f : value > max ? max : value;
-		}
+        /// <summary>
+        /// Single 値を 0.0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static float Clamp(this float value, float max)
+        {
+            return value < 0.0f ? 0.0f : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// Single 値を 0.0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <param name="min">範囲の最小値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static float Clamp (this float value, float max, float min)
-		{
-			return value < min ? min : value > max ? max : value;
-		}
+        /// <summary>
+        /// Single 値を 0.0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <param name="min">範囲の最小値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static float Clamp(this float value, float max, float min)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// float 値を単純に Int32 値に変換します。
-		/// </summary>
-		/// <param name="value">変換される float 値。</param>
-		/// <returns>変換された Int32 値。</returns>
-		public static int ToInt32(this float value)
-		{
-			return (int)value;
-		}
+        /// <summary>
+        /// Int32 値を 0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static int Clamp(this int value, int max)
+        {
+            return value < 0 ? 0 : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// float 値を 0 から指定された数値の範囲で Int32 値に変換します。
-		/// </summary>
-		/// <param name="value">変換される float 値。</param>
-		/// <param name="max">変換の上限となる 最大値。</param>
-		/// <returns>変換された Int32 値。</returns>
-		public static int ToInt32(this float value, int max)
-		{
-			int tmp = (int)value;
-			return tmp < 0 ? 0 : tmp > max ? max : tmp;
-		}
+        /// <summary>
+        /// Int32 値を 0.0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <param name="min">範囲の最小値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static int Clamp(this int value, int max, int min)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// float 値を指定された数値の範囲で Int32 値に変換します。
-		/// </summary>
-		/// <param name="value">変換される float 値。</param>
-		/// <param name="max">変換の上限となる 最大値。</param>
-		/// <param name="min">変換の下限となる 最小値。</param>
-		/// <returns>変換された Int32 値。</returns>
-		public static int ToInt32(this float value, int max, int min)
-		{
-			int tmp = (int)value;
-			return tmp < min ? min : tmp > max ? max : tmp;
-		}
+        /// <summary>
+        /// Int16 値を 0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static short Clamp(this short value, short max)
+        {
+            return value < (short)0 ? (short)0 : value > max ? max : value;
+        }
 
-		/// <summary>
-		/// Int32 値を 0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static int Clamp(this int value, int max)
-		{
-			return value < 0 ? 0 : value > max ? max : value;
-		}
+        /// <summary>
+        /// Int16 値を 0.0 から指定された数値の範囲でクランプします。
+        /// </summary>
+        /// <param name="value">クランプされる値。</param>
+        /// <param name="max">範囲の最大値。</param>
+        /// <param name="min">範囲の最小値。</param>
+        /// <returns>クランプされた値。</returns>
+        public static short Clamp(this short value, short max, short min)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+        #endregion
 
-		/// <summary>
-		/// Int32 値を 0.0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <param name="min">範囲の最小値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static int Clamp (this int value, int max, int min)
-		{
-			return value < min ? min : value > max ? max : value;
-		}
+        #region ToInt32
+        /// <summary>
+        /// float 値を単純に Int32 値に変換します。
+        /// </summary>
+        /// <param name="value">変換される float 値。</param>
+        /// <returns>変換された Int32 値。</returns>
+        public static int ToInt32(this float value)
+        {
+            return (int)value;
+        }
 
-		/// <summary>
-		/// Int16 値を 0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static short Clamp (this short value, short max)
-		{
-			return value < (short)0 ? (short)0 : value > max ? max : value;
-		}
+        /// <summary>
+        /// float 値を 0 から指定された数値の範囲で Int32 値に変換します。
+        /// </summary>
+        /// <param name="value">変換される float 値。</param>
+        /// <param name="max">変換の上限となる 最大値。</param>
+        /// <returns>変換された Int32 値。</returns>
+        public static int ToInt32(this float value, int max)
+        {
+            int tmp = (int)value;
+            return tmp < 0 ? 0 : tmp > max ? max : tmp;
+        }
 
-		/// <summary>
-		/// Int16 値を 0.0 から指定された数値の範囲でクランプします。
-		/// </summary>
-		/// <param name="value">クランプされる値。</param>
-		/// <param name="max">範囲の最大値。</param>
-		/// <param name="min">範囲の最小値。</param>
-		/// <returns>クランプされた値。</returns>
-		public static short Clamp (this short value, short max, short min)
-		{
-			return value < min ? min : value > max ? max : value;
-		}
+        /// <summary>
+        /// float 値を指定された数値の範囲で Int32 値に変換します。
+        /// </summary>
+        /// <param name="value">変換される float 値。</param>
+        /// <param name="max">変換の上限となる 最大値。</param>
+        /// <param name="min">変換の下限となる 最小値。</param>
+        /// <returns>変換された Int32 値。</returns>
+        public static int ToInt32(this float value, int max, int min)
+        {
+            int tmp = (int)value;
+            return tmp < min ? min : tmp > max ? max : tmp;
+        }
+        #endregion
+        #endregion
 	}
 }
