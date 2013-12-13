df4790173a7fc272236c2b177b95bf6e4fa410ff
Tomona Nanase <nanase@users.noreply.github.com>
2013-12-14 02:18:39 +0900

    uxBenchmarkプロジェクトを追加

---
+++ uxBenchmark/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using ux;
+using ux.Component;
+
+namespace uxBenchmark
+{
+    public class Program
+    {
+        const int samplingRate = 44100;
+        const int bufferSize = 1024;
+        const int iterate = 3000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ux Benchmark");
+            Console.WriteLine("Copyright (c) 2013 Tomona Nanase");
+
+            Console.WriteLine();
+            Console.Write("Press any key to start benchmark...");
+            Console.ReadLine();
+
+            var sw = Stopwatch.StartNew();
+            MuteTest();
+            DefaultFMTest();
+            DefaultSquareTest();
+
+            sw.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("Complete in {0:mm\\:ss}", sw.Elapsed);
+            Console.ReadLine();
+        }
+
+        static void MuteTest()
+        {
+            TimeSpan result;
+
+            Console.WriteLine();
+            Console.WriteLine("[MuteTest] for {0}", iterate);
+
+            Console.WriteLine("Part  Total  PerCall      PerSecond");
+
+            foreach (int partCount in new[] { 16, 64, 128, 256 })
+            {
+                Console.Write("{0,-4}  ", partCount);
+                result = MuteTest(partCount);
+                Console.WriteLine("{0:mm\\:ss}  {1:ss\\.fffffff}s  {2:f3}", result, result.Divide(iterate), 1.0 / result.Divide(iterate).TotalSeconds);
+            }
+        }
+
+        static TimeSpan MuteTest(int partCount)
+        {
+            Master master = new Master(samplingRate, partCount);
+            float[] buffer = new float[bufferSize];
+
+            master.Play();
+            master.Read(buffer, 0, bufferSize);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            sw.Stop();
+            sw.Restart();
+            sw.Restart();
+
+            for (int i = 0; i < iterate; i++)
+            {
+                master.Read(buffer, 0, bufferSize);
+            }
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+
+        static void DefaultFMTest()
+        {
+            TimeSpan result;
+
+            Console.WriteLine();
+            Console.WriteLine("[DefaultFMTest] for {0}", iterate);
+
+            Console.WriteLine("Part  Total  PerCall      PerSecond");
+
+            foreach (int partCount in new[] { 16, 64, 128, 256 })
+            {
+                Console.Write("{0,-4}  ", partCount);
+                result = DefaultFMTest(partCount);
+                Console.WriteLine("{0:mm\\:ss}  {1:ss\\.fffffff}s  {2:f3}", result, result.Divide(iterate), 1.0 / result.Divide(iterate).TotalSeconds);
+            }
+        }
+
+        static TimeSpan DefaultFMTest(int partCount)
+        {
+            Master master = new Master(samplingRate, partCount);
+            float[] buffer = new float[bufferSize];
+
+            master.PushHandle(new Handle(0, HandleType.Waveform, (int)WaveformType.FM), Enumerable.Range(1, partCount));
+            master.PushHandle(new Handle(0, HandleType.NoteOn, 60, 1.0f), Enumerable.Range(1, partCount));
+
+            master.Play();
+            master.Read(buffer, 0, bufferSize);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            sw.Stop();
+            sw.Restart();
+            sw.Restart();
+
+            for (int i = 0; i < iterate; i++)
+            {
+                master.Read(buffer, 0, bufferSize);
+            }
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+
+        static void DefaultSquareTest()
+        {
+            TimeSpan result;
+
+            Console.WriteLine();
+            Console.WriteLine("[DefaultSquareTest] for {0}", iterate);
+
+            Console.WriteLine("Part  Total  PerCall      PerSecond");
+
+            foreach (int partCount in new[] { 16, 64, 128, 256 })
+            {
+                Console.Write("{0,-4}  ", partCount);
+                result = DefaultSquareTest(partCount);
+                Console.WriteLine("{0:mm\\:ss}  {1:ss\\.fffffff}s  {2:f3}", result, result.Divide(iterate), 1.0 / result.Divide(iterate).TotalSeconds);
+            }
+        }
+
+        static TimeSpan DefaultSquareTest(int partCount)
+        {
+            Master master = new Master(samplingRate, partCount);
+            float[] buffer = new float[bufferSize];
+
+            master.PushHandle(new Handle(0, HandleType.Waveform, (int)WaveformType.Square), Enumerable.Range(1, partCount));
+            master.PushHandle(new Handle(0, HandleType.NoteOn, 60, 1.0f), Enumerable.Range(1, partCount));
+
+            master.Play();
+            master.Read(buffer, 0, bufferSize);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            sw.Stop();
+            sw.Restart();
+            sw.Restart();
+
+            for (int i = 0; i < iterate; i++)
+            {
+                master.Read(buffer, 0, bufferSize);
+            }
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+
+    }
+
+    public static class Extension
+    {
+        public static TimeSpan Divide(this TimeSpan a, double b)
+        {
+            return new TimeSpan((long)Math.Round(a.Ticks / b));
+        }
+    }
+}
---
+++ uxBenchmark/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("uxBenchmark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("uxBenchmark")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("771437dc-598f-4e68-8c2b-1af2fe0789d5")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
