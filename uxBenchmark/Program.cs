6aa14002c0fbe30b4d172f017c873bc933b075b9
Tomona Nanase <nanase@users.noreply.github.com>
2013-12-15 10:18:54 +0900

    FullFMTestメソッドを追加

--- uxBenchmark/Program.cs
+++ uxBenchmark/Program.cs
@@ -25,6 +25,7 @@ namespace uxBenchmark
             MuteTest();
             DefaultFMTest();
             DefaultSquareTest();
+            FullFMTest();
 
             sw.Stop();
 
@@ -168,6 +169,89 @@ namespace uxBenchmark
         }
         #endregion
 
+        #region FullFMTest
+        static void FullFMTest()
+        {
+            TimeSpan result;
+
+            Console.WriteLine();
+            Console.WriteLine("[FullFMTest] for {0}", iterate);
+
+            Console.WriteLine("Part  Total  PerCall      PerSecond");
+
+            foreach (int partCount in new[] { 16, 64, 128, 256 })
+            {
+                Console.Write("{0,-4}  ", partCount);
+                result = FullFMTest(partCount);
+                Console.WriteLine("{0:mm\\:ss}  {1:ss\\.fffffff}s  {2:f3}", result, result.Divide(iterate), 1.0 / result.Divide(iterate).TotalSeconds);
+            }
+        }
+
+        static TimeSpan FullFMTest(int partCount)
+        {
+            Master master = new Master(samplingRate, partCount);
+            float[] buffer = new float[bufferSize];
+
+            var range = Enumerable.Range(1, partCount).ToArray();
+
+            master.PushHandle(new Handle(0, HandleType.Waveform, (int)WaveformType.FM), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)FMOperate.Send0, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)FMOperate.Send0, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)FMOperate.Send0, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)FMOperate.Send0, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)FMOperate.Send1, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)FMOperate.Send1, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)FMOperate.Send1, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)FMOperate.Send1, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)FMOperate.Send2, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)FMOperate.Send2, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)FMOperate.Send2, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)FMOperate.Send2, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)FMOperate.Send3, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)FMOperate.Send3, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)FMOperate.Send3, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)FMOperate.Send3, 0.1f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)FMOperate.Out, 0.25f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)FMOperate.Out, 0.25f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)FMOperate.Out, 0.25f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)FMOperate.Out, 0.25f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)EnvelopeOperate.A, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)EnvelopeOperate.A, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)EnvelopeOperate.A, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)EnvelopeOperate.A, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)EnvelopeOperate.P, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)EnvelopeOperate.P, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)EnvelopeOperate.P, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)EnvelopeOperate.P, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)EnvelopeOperate.D, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)EnvelopeOperate.D, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)EnvelopeOperate.D, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)EnvelopeOperate.D, 5), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op0 | (int)EnvelopeOperate.S, 0.5f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op1 | (int)EnvelopeOperate.S, 0.5f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op2 | (int)EnvelopeOperate.S, 0.5f), range);
+            master.PushHandle(new Handle(0, HandleType.Edit, (int)FMOperate.Op3 | (int)EnvelopeOperate.S, 0.5f), range);
+            master.PushHandle(new Handle(0, HandleType.NoteOn, 60, 1.0f), range);
+
+            master.Play();
+            master.Read(buffer, 0, bufferSize);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            sw.Stop();
+            sw.Restart();
+            sw.Restart();
+
+            for (int i = 0; i < iterate; i++)
+            {
+                master.Read(buffer, 0, bufferSize);
+            }
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+        #endregion
     }
 
     public static class Extension
