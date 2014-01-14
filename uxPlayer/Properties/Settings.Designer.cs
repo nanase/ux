fae77c595e50a15ba6423ef51b4954f1b768b05e
Tomona Nanase <nanase@users.noreply.github.com>
2014-01-14 13:07:08 +0900

    PlayerPresetの初期値を修正

--- uxPlayer/Properties/Settings.Designer.cs
+++ uxPlayer/Properties/Settings.Designer.cs
@@ -75,7 +75,7 @@ namespace uxPlayer.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>ux_presets.xml</string>\r\n</ArrayOfString>")]
+            "tring>ux_preset.xml</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection PlayerPresets {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["PlayerPresets"]));
