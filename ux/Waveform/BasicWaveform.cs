307b6153c002648ded3d7f9c90c60f2d695c4c3a
Tomona Nanase <nanase@users.noreply.github.com>
2014-03-18 16:38:59 +0900

    BasicWaveform.csをCachedWaveformに対応

--- ux/Waveform/BasicWaveform.cs
+++ ux/Waveform/BasicWaveform.cs
@@ -29,26 +29,36 @@ using ux.Utils;
 
 namespace ux.Waveform
 {
+    struct BaseWaveformCache : CacheObject<BaseWaveformCache>
+    {
+        public float[] DataValue { get; set; }
+
+        public int Step { get; private set; }
+
+        public int Length { get { return this.DataValue.Length; } }
+
+        public BaseWaveformCache(int step)
+            : this()
+        {
+            this.Step = step;
+        }
+
+        public bool Equals(BaseWaveformCache other)
+        {
+            return this.Step == other.Step;
+        }
+
+        public bool CanResize(BaseWaveformCache other)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
     /// <summary>
     /// 矩形波を生成する波形ジェネレータクラスです。
     /// </summary>
-    class Square : StepWaveform
+    class Square : CachedWaveform<BaseWaveformCache>
     {
-        #region -- Private Fields --
-        private static readonly LinkedList<Tuple<int, float[]>> cache = new LinkedList<Tuple<int, float[]>>();
-        private const int MaxCacheSize = 32;
-        #endregion
-
-        #region -- Constructors --
-        /// <summary>
-        /// 新しい Square クラスのインスタンスを初期化します。
-        /// </summary>
-        public Square()
-            : base()
-        {
-        }
-        #endregion
-
         #region -- Public Methods --
         /// <summary>
         /// パラメータを指定してこの波形の設定値を変更します。
@@ -62,6 +72,7 @@ namespace ux.Waveform
                 case BasicWaveformOperate.Duty:
                     this.GenerateStep(data2.Clamp(1.0f, 0.0f));
                     break;
+
                 default:
                     base.SetParameter(data1, data2);
                     break;
@@ -77,6 +88,28 @@ namespace ux.Waveform
         }
         #endregion
 
+        #region -- Protected Methods --
+        protected override byte[] Generate(BaseWaveformCache parameter)
+        {
+            bool reverse = parameter.Step < 0;
+            int onTime = (reverse) ? -parameter.Step : parameter.Step;
+
+            byte[] l = new byte[onTime + 1];
+
+            if (reverse)
+            {
+                // 10, 110, 1110, ...
+                for (int i = 0; i < onTime; i++)
+                    l[i] = (byte)1;
+            }
+            else
+                // 10, 100, 1000, ...
+                l[0] = (byte)1;
+
+            return l;
+        }
+        #endregion
+
         #region -- Private Methods --
         /// <summary>
         /// デューティ比を指定して新しい矩形波を設定します。
@@ -89,40 +122,13 @@ namespace ux.Waveform
 
             int onTime = (int)(1.0f / (duty <= 0.5f ? duty : (1.0f - duty))) - 1;
 
-            if (onTime > StepWaveform.MaxDataSize)
+            if (onTime < 0 || onTime > StepWaveform.MaxDataSize)
                 return;
 
-            for (var now = cache.First; now != null; now = now.Next)
-            {
-                if (now.Value.Item1 == onTime)
-                {
-                    Tuple<int, float[]> nc = now.Value;
-                    this.value = nc.Item2;
-                    this.length = nc.Item2.Length;
-                    return;
-                }
-            }
+            if (duty > 0.5f)
+                onTime = -onTime;
 
-            byte[] l = new byte[onTime + 1];
-
-            if (duty <= 0.5f)
-            {
-                // 10, 100, 1000, ...
-                l[0] = (byte)1;
-            }
-            else
-            {
-                // 10, 110, 1110, ...
-                for (int i = 0; i < onTime; i++)
-                    l[i] = (byte)1;
-            }
-
-            this.SetStep(l);
-
-            cache.AddFirst(new Tuple<int, float[]>(onTime, this.value));
-
-            if (cache.Count > Square.MaxCacheSize)
-                cache.RemoveLast();
+            this.Cache(new BaseWaveformCache(onTime));
         }
         #endregion
     }
@@ -130,23 +136,8 @@ namespace ux.Waveform
     /// <summary>
     /// 擬似三角波を生成する波形ジェネレータクラスです。
     /// </summary>
-    class Triangle : StepWaveform
+    class Triangle : CachedWaveform<BaseWaveformCache>
     {
-        #region -- Private Fields --
-        private static readonly LinkedList<Tuple<int, float[]>> cache = new LinkedList<Tuple<int, float[]>>();
-        private const int MaxCacheSize = 32;
-        #endregion
-
-        #region -- Constructors --
-        /// <summary>
-        /// 新しい Triangle クラスのインスタンスを初期化します。
-        /// </summary>
-        public Triangle()
-            : base()
-        {
-        }
-        #endregion
-
         #region -- Public Methods --
         /// <summary>
         /// パラメータを指定してこの波形の設定値を変更します。
@@ -175,6 +166,21 @@ namespace ux.Waveform
         }
         #endregion
 
+        #region -- Protected Methods --
+        protected override byte[] Generate(BaseWaveformCache parameter)
+        {
+            byte[] l = new byte[parameter.Step * 2];
+
+            for (int i = 0; i < parameter.Step; i++)
+                l[i] = (byte)i;
+
+            for (int i = parameter.Step; i < parameter.Step * 2; i++)
+                l[i] = (byte)(parameter.Step * 2 - i - 1);
+
+            return l;
+        }
+        #endregion
+
         #region -- Private Methods --
         /// <summary>
         /// ステップ数を指定して新しい擬似三角波を設定します。
@@ -185,32 +191,7 @@ namespace ux.Waveform
             if (step <= 0 || step > 256)
                 return;
 
-            for (var now = cache.First; now != null; now = now.Next)
-            {
-                if (now.Value.Item1 == step)
-                {
-                    Tuple<int, float[]> nc = now.Value;
-
-                    this.value = nc.Item2;
-                    this.length = nc.Item2.Length;
-                    return;
-                }
-            }
-
-            byte[] l = new byte[step * 2];
-
-            for (int i = 0; i < step; i++)
-                l[i] = (byte)i;
-
-            for (int i = step; i < step * 2; i++)
-                l[i] = (byte)(step * 2 - i - 1);
-
-            this.SetStep(l);
-
-            cache.AddFirst(new Tuple<int, float[]>(step, this.value));
-
-            if (cache.Count > Triangle.MaxCacheSize)
-                cache.RemoveLast();
+            this.Cache(new BaseWaveformCache(step));
         }
         #endregion
     }
